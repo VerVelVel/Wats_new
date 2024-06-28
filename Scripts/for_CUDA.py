4ac11af0dfc58373828521b33f6e1458b25ee884
VerVelVel <vera.velikoborets@gmail.com>
2024-06-28 10:23:22 +0300

    delete

--- Scripts/for_CUDA.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print("CUDA available: ", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("CUDA device count: ", torch.cuda.device_count())
-    print("CUDA device name: ", torch.cuda.get_device_name(0))
