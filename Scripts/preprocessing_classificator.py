9e40616295f623889a4b3dbc3d594f69f7f7b38e
VerVelVel <vera.velikoborets@gmail.com>
2024-07-01 15:17:44 +0300

    last bot

--- Scripts/preprocessing_classificator.py
+++ Scripts/preprocessing_classificator.py
@@ -1,18 +1,30 @@
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+import pickle
 import pandas as pd
 import re
 import string
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import torch
-from transformers import pipeline
-from datasets import Dataset
+from torch.nn.functional import softmax
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
 from functools import lru_cache
-from concurrent.futures import ThreadPoolExecutor
-import requests
 import time
 
-# Загрузка данных и предобработка
+# Загрузка модели и токенизатора из pkl файла
+# scripts_dir = os.path.dirname(os.path.abspath(__file__))  # Получаем текущую директорию скрипта
+# filepath = os.path.join(scripts_dir, "model_tokenizer.pkl")
+# with open(filepath, "rb") as f:
+#     model, tokenizer = pickle.load(f)
+
+# # Перемещение модели на GPU, если доступно
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model.to(device)
+
+# Предобработка текста
 def preprocess_data(df):
+    start_time = time.time()  
     df = df.dropna(subset=['Content'])
     stop_words = set(stopwords.words('english')).union(set(stopwords.words('russian')))
     lemmatizer = WordNetLemmatizer()
@@ -22,7 +34,7 @@ def preprocess_data(df):
         text = re.sub('<.*?>', '', text)
         text = re.sub(r'[^\w\s,]', '', text)
         text = ''.join([c for c in text if c not in string.punctuation])
-        text = re.sub(r'\d+', '<NUM>', text)
+        # text = re.sub(r'\d+', '<NUM>', text)
         text = re.sub(r'http\S+|www\S+|https\S+', '<URL>', text, flags=re.MULTILINE)
         text = re.sub(r'@\w+', '<USER>', text)
         text = ' '.join([word for word in text.split() if word not in stop_words])
@@ -30,83 +42,118 @@ def preprocess_data(df):
         text = re.sub(r'\s+', ' ', text).strip()
         return text
 
-    df.loc[:, 'clean_text'] = df['Content'].apply(preprocess_text)
-    df['message_channel'] = df['clean_text'] + ' ' + df['Source'].copy()
-    return df
-
-
-API_URL = "https://api-inference.huggingface.co/models/MoritzLaurer/deberta-v3-large-zeroshot-v2.0"
-headers = {"Authorization": "Bearer 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"}
-
-def query(payload, max_retries=3, retry_delay=5):
-    retries = 0
-    while retries < max_retries:
-        try:
-            response = requests.post(API_URL, headers=headers, json=payload)
-            if response.status_code == 200:
-                return response.json()
-            else:
-                print(f"Request failed with status code {response.status_code}. Retrying...")
-        except requests.exceptions.RequestException as e:
-            print(f"Request failed: {str(e)}. Retrying...")
-        
-        retries += 1
-        time.sleep(retry_delay)
-    
-    return {"error": f"Failed to get response after {max_retries} retries"}
-
-
-def classify_text(df):
-    candidate_labels = ["технологии", "политика", "спорт", "экономика", "развлечения", "здоровье", "образование", "мода", "инциденты и катастрофы"]
-
-    def classify_row(row):
-        payload = {
-            "inputs": row['message_channel'],
-            "parameters": {"candidate_labels": candidate_labels}
-        }
-        result = query(payload)
-
-        if 'scores' not in result:
-            print("Result without scores:", result)
-            return "error"
-
-        highest_score_index = result['scores'].index(max(result['scores']))
-        most_likely_label = result['labels'][highest_score_index]
-        return most_likely_label
-
-    with ThreadPoolExecutor() as executor:
-        results = []
-        for result in executor.map(classify_row, df.to_dict('records')):
-            results.append(result)
-            time.sleep(1)  # Добавление задержки в 1 секунду между запросами
-        df['Category'] = results
-
+    df['clean_text'] = df['Content'].apply(preprocess_text)
+    df['message_channel'] = df['clean_text'] + ' ' + df['Source']
+    end_time = time.time()  # Засекаем время окончания выполнения функции
+    print(f"TIME Preprocessing took {end_time - start_time} seconds") 
     return df
 
 # # # Инициализация модели для zero-shot классификации с кэшированием
-# @lru_cache(maxsize=1)
-# def init_classifier():
-#     # Проверка доступности CUDA до инициализации модели
-#     device = 0 if torch.cuda.is_available() else -1
-#     if device == -1:
-#         print("CUDA is not available. Using CPU instead.")
-#     classifier = pipeline("zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0", device=device)
-#     return classifier
+@lru_cache(maxsize=1)
+def init_classifier():
+    # Проверка доступности CUDA до инициализации модели
+    device = 0 if torch.cuda.is_available() else -1
+    if device == -1:
+        print("CUDA is not available. Using CPU instead.")
+    classifier = pipeline("zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0", device=device)
+    return classifier
 
-# # Функция для классификации текста
-# def classify_text_1(df, classifier):
-#     candidate_labels = ["технологии", "политика", "спорт", "экономика", "развлечения", "здоровье", "образование", "мода", "происшествия"]
-    
+# Функция для классификации текста
+def classify_text(df, classifier):
+    start_clas_time = time.time()
+    candidate_labels = ["технологии", "политика", "спорт", "экономика", "развлечения", "здоровье", "образование", "мода", "инциденты и катастрофы"]
+
+    def classify_batch(batch):
+        messages = [row['message_channel'] for row in batch]
+        results = classifier(messages, candidate_labels)
+        return [result['labels'][result['scores'].index(max(result['scores']))] for result in results]
+
+    # Adjust batch size based on your system's capabilities and data characteristics
+    batch_size = 16  # Experiment with different batch sizes
+    num_batches = (len(df) + batch_size - 1) // batch_size
+
+    classified_results = []
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for i in range(num_batches):
+            start_idx = i * batch_size
+            end_idx = min((i + 1) * batch_size, len(df))
+            batch = df.iloc[start_idx:end_idx].to_dict('records')
+            futures.append(executor.submit(classify_batch, batch))
+
+        for future in as_completed(futures):
+            batch_results = future.result()
+            classified_results.extend(batch_results)
+
+    df['Category'] = classified_results
+    end_clf_time = time.time()
+    print(f"TIME Classification took {end_clf_time - start_clas_time} seconds")
+    return df
+# # Классификация текста
+# def classify_text(df):
+#     candidate_labels = ["технологии", "политика", "спорт", "экономика", "развлечения", "здоровье", "образование", "мода", "инциденты и катастрофы"]
+
 #     def classify_row(row):
-#         result = classifier(row['message_channel'], candidate_labels)
-#         highest_score_index = result['scores'].index(max(result['scores']))
-#         most_likely_label = result['labels'][highest_score_index]
-#         return most_likely_label
+#         inputs = tokenizer(row['message_channel'], return_tensors="pt", padding=True, truncation=True).to(device)
+#         with torch.no_grad():
+#             outputs = model(**inputs)
+#             scores = softmax(outputs.logits, dim=1).cpu().numpy().flatten()
+#             highest_score_index = scores.argmax()
+#             most_likely_label = candidate_labels[highest_score_index]
+#             return most_likely_label
 
 #     with ThreadPoolExecutor() as executor:
-#         df['Category'] = list(executor.map(classify_row, df.to_dict('records')))
+#         results = list(executor.map(classify_row, df.to_dict('records')))
+#     df['Category'] = results
+#     return df
+
+# async def query_async(session, payload, max_retries=3):
+#     for attempt in range(max_retries):
+#         try:
+#             async with session.post(API_URL, headers=headers, json=payload) as response:
+#                 result = await response.json()
+#                 if 'scores' in result:
+#                     return result
+#                 else:
+#                     print(f"Result without scores on attempt {attempt + 1} of {max_retries}")
+#         except aiohttp.ClientError as e:
+#             print(f"ClientError: {e}. Attempt {attempt + 1} of {max_retries}")
+#         except Exception as e:
+#             print(f"Unexpected error: {e}. Attempt {attempt + 1} of {max_retries}")
+#         await asyncio.sleep(2 ** attempt)  # Exponential backoff
+
+#     return {"error": f"Failed to get response after {max_retries} retries"}
+
+# async def classify_row_async(session, row, candidate_labels, max_retries=3):
+#     payload = {
+#         "inputs": row['message_channel'],
+#         "parameters": {"candidate_labels": candidate_labels}
+#     }
+#     result = await query_async(session, payload, max_retries)
+
+#     if 'scores' not in result:
+#         print("Result without scores:", result)
+#         return "error"
+
+#     highest_score_index = result['scores'].index(max(result['scores']))
+#     most_likely_label = result['labels'][highest_score_index]
+#     return most_likely_label
+
+# async def classify_text_async(df):
+#     candidate_labels = ["технологии", "политика", "спорт", "экономика", "развлечения", "здоровье", "образование", "мода", "инциденты и катастрофы"]
+
+#     async with aiohttp.ClientSession() as session:
+#         tasks = [classify_row_async(session, row, candidate_labels) for row in df.to_dict('records')]
+#         results = []
+#         for future in asyncio.as_completed(tasks):
+#             result = await future
+#             results.append(result)
+#             await asyncio.sleep(0.5)  # Добавление небольшой задержки между запросами
+#         df['Category'] = results
 
 #     return df
+
+
 
 # def query(payload, max_retries=5, retry_delay=5):
 #     retries = 0
---
+++ Scripts/test_bot_butt.py
@@ -0,0 +1,143 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+import asyncio
+import os
+from aiogram.utils import executor
+import pandas as pd
+from parser_1 import parse_channels 
+from preprocessing_classificator import preprocess_data, classify_text, init_classifier
+# from preprocessing_classificator import preprocess_data, classify_text_async
+
+# Настройки бота aiogram
+bot_token = '7466597015:AAEsbquKsmzi6Sr_YtNv3SCkeM3uukm1FT4'
+bot = Bot(token=bot_token)
+dp = Dispatcher(bot)
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Команды для кнопок
+start_keyboard = types.ReplyKeyboardMarkup(keyboard=[[types.KeyboardButton(text="/start")]], resize_keyboard=True)
+
+parse_keyboard = types.ReplyKeyboardMarkup(keyboard=[[types.KeyboardButton(text="Начнем!")]], resize_keyboard=True)
+
+period_keyboard = types.ReplyKeyboardMarkup(keyboard=[[types.KeyboardButton(text="Выбрать период")],
+                                                      [types.KeyboardButton(text="Ввести конкретную дату")],
+                                                      [types.KeyboardButton(text="К вводу каналов")]], resize_keyboard=True)
+
+period_options_keyboard = types.ReplyKeyboardMarkup(keyboard=[[types.KeyboardButton(text="1 день")],
+                                                              [types.KeyboardButton(text="3 дня")], [types.KeyboardButton(text="Назад")]], resize_keyboard=True)
+
+# Состояния для FSM
+class ParseStates:
+    WAITING_FOR_LINKS = 1
+    WAITING_FOR_PERIOD = 2
+    WAITING_FOR_DATE = 3
+
+# Переменная для хранения данных пользователя
+user_data = {}
+
+# Команда /start
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я здесь, чтобы помочь тебе быть в курсе всех новостей 🤓. Давай начинать!", reply_markup=parse_keyboard)
+    logger.info("Пользователь начал работу с ботом.")
+
+# Команда "поехали!"
+@dp.message_handler(lambda message: message.text == "Начнем!")
+async def parse_command(message: types.Message):
+    await message.answer("Введите ссылки на Telegram-каналы, новости из которых вы хотите узнать, разделенные запятой:", reply_markup=types.ReplyKeyboardRemove())
+    user_data[message.from_user.id] = {'state': ParseStates.WAITING_FOR_LINKS}
+    logger.info(f"Пользователь {message.from_user.id} ввел команду /parse")
+
+# Обработка введенных ссылок
+@dp.message_handler(lambda message: user_data.get(message.from_user.id, {}).get('state') == ParseStates.WAITING_FOR_LINKS)
+async def handle_links(message: types.Message):
+    user_data[message.from_user.id]['links'] = message.text
+    user_data[message.from_user.id]['state'] = ParseStates.WAITING_FOR_PERIOD
+    await message.answer("Выберите период поиска новостей или введите конкретную дату:", reply_markup=period_keyboard)
+    logger.info(f"Пользователь {message.from_user.id} ввел ссылки: {message.text}")
+
+# Обработка выбора периода
+@dp.message_handler(lambda message: message.text in ["Выбрать период", "Ввести конкретную дату", "1 день", "3 дня", "Назад", "К вводу каналов"])
+async def handle_period_choice(message: types.Message):
+    user_id = message.from_user.id
+    logger.info(f"Пользователь {user_id} выбрал опцию: {message.text}")
+    logger.info(f"Текущее состояние пользователя: {user_data.get(user_id, {}).get('state')}")
+
+    if message.text == "Выбрать период":
+        await message.answer("Выберите период для парсинга:", reply_markup=period_options_keyboard)
+    elif message.text == "Ввести конкретную дату":
+        await message.answer("Введите дату в формате ГГГГ-ММ-ДД:")
+        user_data[user_id]['state'] = ParseStates.WAITING_FOR_DATE
+    elif message.text in ["1 день", "3 дня"]:
+        period_days = 3 if message.text == "3 дня" else 1
+        links = user_data[user_id]['links'].split(',')
+        await start_parsing(message, links, period_days=period_days)
+    elif message.text == "Назад":
+        await message.answer("Выберите период поиска новостей или введите конкретную дату:", reply_markup=period_keyboard)
+    elif message.text == "К вводу каналов":
+        user_data[user_id]['state'] = ParseStates.WAITING_FOR_LINKS
+        await message.answer("Введите ссылки на Telegram-каналы, новости из которых вы хотите узнать, разделенные запятой:",
+                             reply_markup=types.ReplyKeyboardRemove())
+
+# Обработка введенной конкретной даты
+@dp.message_handler(lambda message: user_data.get(message.from_user.id, {}).get('state') == ParseStates.WAITING_FOR_DATE)
+async def handle_date(message: types.Message):
+    user_id = message.from_user.id
+    date = message.text
+    try:
+        specific_date = pd.to_datetime(date)  # Проверка корректности даты
+    except ValueError:
+        await message.answer("Неверный формат даты. Попробуйте снова.")
+        return
+    
+    links = user_data[user_id]['links'].split(',')
+    await start_parsing(message, links, specific_date=specific_date)
+    # user_data[user_id]['state'] = None  # Очищаем состояние после парсинга
+    logger.info(f"Парсинг завершен для пользователя {user_id}")
+
+async def start_parsing(message: types.Message, links: list, period_days=None, specific_date=None):
+    user_id = message.from_user.id
+
+    await message.answer("Начинаем парсинг...", reply_markup=types.ReplyKeyboardRemove())
+    logger.info(f"Начинаем парсинг для пользователя {user_id} с периодом {period_days} дней и ссылками: {links}")
+    success = await parse_channels(links, period_days=period_days, specific_date=specific_date)
+    logger.info(f"Результат парсинга для пользователя {user_id}: {'успех' if success else 'неудача'}")
+
+    if success:
+        await message.answer("Парсинг завершен. Начинаем классификацию...")
+
+        # Классификация
+        scripts_dir = os.path.dirname(os.path.abspath(__file__))  # Получаем текущую директорию скрипта
+        filepath = os.path.join(scripts_dir, 'output.csv')
+        if os.path.exists(filepath):
+            df = pd.read_csv(filepath)
+            df = preprocess_data(df)
+            classifier = init_classifier()
+            df = classify_text(df, classifier)
+
+            # Сохранение данных с классификацией
+            df.to_csv(filepath, index=False)
+            await message.answer(f"Классификация завершена. Данные сохранены в файле {filepath}.")
+            logger.info(f"Классификация завершена. Данные сохранены в файле {filepath}.")
+        else:
+            await message.answer(f"Ошибка: файл {filepath} не найден.")
+            logger.error(f"Ошибка: файл {filepath} не найден.")
+    else:
+        await message.answer("Парсинг завершен. Нет данных для сохранения.")
+        logger.info("Парсинг завершен. Нет данных для сохранения.")
+
+    user_data.pop(user_id, None)  # Очистка данных пользователя
+
+# Запуск бота
+if __name__ == '__main__':
+    logger.info("Телеграм клиент запущен")
+    executor.start_polling(dp, skip_updates=True)
+    logger.info("Бот запущен")
+#Выбор из имеющихся категорий
+# await message.answer("Выберите категорию новостей:", reply_markup=generate_category_keyboard(classified_df['category'].unique()))
+#             user_data[user_id]['state'] = ParseStates.CHOOSING_CATEGORY
